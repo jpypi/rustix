057a534a24c950428f240ad7f748fcbcc0ccc9f0
James Jenkins <applesmacs@yahoo.com>
2017-10-10 13:47:36 -0400

    Fix up/down voting regex to be more robust

--- src/services/upvote/mod.rs
+++ src/services/upvote/mod.rs
@@ -32,24 +32,23 @@ impl<'a> Node<'a> for UpvoteTracker {
         let event = event.raw_event;
         if event.type_ == "m.room.message" &&
             event.content["msgtype"] == "m.text" {
-            let up_re = Regex::new(r"([^ ]+|\(.+?\))\+\+").unwrap();
-            let dn_re = Regex::new(r"([^ ]+|\(.+?\))--").unwrap();
-
             let body = event.content["body"].as_str().unwrap();
-            //println!("found karma for: {}", ent);
 
             let mut votes: HashMap<String, VoteCount> = HashMap::new();
 
-            for cap in up_re.captures_iter(body) {
-                let ent = cap[1].trim_matches(|c| c == '(' || c == ')').to_string();
-                let e = votes.entry(ent).or_insert(VoteCount{up: 0, down: 0});
-                (*e).up += 1;
-            }
+            let re = Regex::new(r"([^ ]+|\(.+?\))(\+\+|--)").unwrap();
 
-            for cap in dn_re.captures_iter(body) {
+            for cap in re.captures_iter(body) {
                 let ent = cap[1].trim_matches(|c| c == '(' || c == ')').to_string();
                 let e = votes.entry(ent).or_insert(VoteCount{up: 0, down: 0});
-                (*e).down += 1;
+
+                if &cap[2] == "++" {
+                    (*e).up += 1;
+                }
+
+                if &cap[2] == "--" {
+                    (*e).down += 1;
+                }
             }
 
             for (k, v) in votes.iter() {
