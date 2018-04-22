f72544805991242afbf2fa617eabd5cce615b4ab
James Jenkins <applesmacs@yahoo.com>
2018-04-22 19:09:04 -0400

    Clean up join command

--- src/services/join.rs
+++ src/services/join.rs
@@ -12,13 +12,11 @@ impl Join {
 
 impl<'a> Node<'a> for Join {
     fn handle(&mut self, bot: &Bot, event: RoomEvent) {
-        let revent = &event.raw_event;
-        let body = &revent.content["body"].as_str().unwrap();
-        //let sender = &revent.sender;
+        let body = &event.raw_event.content["body"].as_str().unwrap();
         if body.starts_with("join ") {
             let room_id = &body[5..];
             if let Err(e) = bot.join_public(room_id) {
-                let resp = format!("Could not join: {}", &body[5..]);
+                let resp = format!("Could not join: {}", room_id);
                 bot.reply(&event, &resp);
             }
         }
