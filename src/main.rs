e37aa2c5078c3240bfa9ee330c5de19662331084
James Jenkins <applesmacs@yahoo.com>
2018-04-28 02:50:15 -0400

    Re-arrange services and comment main

--- src/main.rs
+++ src/main.rs
@@ -49,42 +49,44 @@ struct Bot {
 
 
 fn main() {
+    // Load config
     let mut f = File::open("config.toml").expect("auth file not found");
     let mut config_data = String::new();
     f.read_to_string(&mut config_data).expect("Couldn't read config.toml");
 
-    let config: Config = toml::from_str(&config_data)
-                         .expect("Bad config.toml");
+    let config: Config = toml::from_str(&config_data).expect("Bad config.toml");
 
+    // Set up a matrix HTTP client
     let mut m = MatrixClient::new(&config.connection.server);
 
     m.login(&config.connection.username,
             &config.connection.password).expect("login failed!");
     m.set_display_name(&config.bot.display_name).unwrap();
 
+    // Create a new bot
     let mut b = bot::Bot::new(&mut m);
 
+    // Register services with the bot
     let sf = b.register_service("self_filter", None, Box::new(SelfFilter::new()));
     let pf = b.register_service("prefix", sf,
                                 Box::new(Prefix::new(config.bot.prefix)));
-    b.register_service("echo", pf, Box::new(Echo::new()));
 
-    b.register_service("show_karma", pf, Box::new(show_karma::ShowKarma::new()));
     b.register_service("karma_tracker", sf, Box::new(KarmaTracker::new()));
 
+    b.register_service("show_karma", pf, Box::new(show_karma::ShowKarma::new()));
+    b.register_service("echo", pf, Box::new(Echo::new()));
     b.register_service("read_quote", pf, Box::new(read_quote::ReadQuote::new()));
     b.register_service("choose", pf, Box::new(Choose::new()));
     b.register_service("roulette", pf, Box::new(Roulette::new()));
     b.register_service("crypto_coin", pf, Box::new(CryptoCoin::new()));
+    b.register_service("try_file", pf, Box::new(TryFile::new()));
 
     let adm = b.register_service("admin", pf,
                                  Box::new(Admin::new(config.bot.admins)));
-
     b.register_service("join", adm, Box::new(Join::new()));
     b.register_service("leave", adm, Box::new(Leave::new()));
     b.register_service("del_quote", adm, Box::new(del_quote::DelQuote::new()));
 
-    b.register_service("try_file", pf, Box::new(TryFile::new()));
-
+    // Start bot main loop
     b.run(&config.bot.rooms);
 }
