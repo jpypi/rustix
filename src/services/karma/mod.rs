8a498d6ad87711601ef87104fc164d135af40789
James Jenkins <applesmacs@yahoo.com>
2018-04-28 22:11:15 -0400

    Improve KarmaTracker; Limit max to prevent spam
    - Move regex to the struct so as to only compile it once

--- src/services/karma/mod.rs
+++ src/services/karma/mod.rs
@@ -11,12 +11,14 @@ use self::backend::Backend;
 
 pub struct KarmaTracker {
     vote_db: Backend,
+    re: Regex,
 }
 
 impl KarmaTracker {
     pub fn new() -> Self {
         Self {
-            vote_db: Backend::new()
+            vote_db: Backend::new(),
+            re: Regex::new(r"([^\- ]+|\(.+?\))(\+\+|--)").unwrap(),
         }
     }
 }
@@ -29,36 +31,35 @@ struct VoteCount {
 impl<'a> Node<'a> for KarmaTracker {
     fn handle(&mut self, bot: &Bot, event: RoomEvent) {
         let event = event.raw_event;
-        if event.type_ == "m.room.message" &&
-            event.content["msgtype"] == "m.text" {
-            let body = event.content["body"].as_str().unwrap();
+        let body = event.content["body"].as_str().unwrap();
 
-            // Don't karma based off anything that is a command to the bot
-            if body.starts_with("!") {
-                return;
+        // Don't karma based off anything that is a command to the bot
+        if body.starts_with("!") {
+            return;
+        }
+
+        let mut votes: HashMap<String, VoteCount> = HashMap::new();
+
+        for cap in self.re.captures_iter(body) {
+            let ent = cap[1].trim_matches(|c| c == '(' || c == ')').to_string();
+            let e = votes.entry(ent).or_insert(VoteCount{up: 0, down: 0});
+
+            if &cap[2] == "++" {
+                (*e).up += 1;
             }
 
-            let mut votes: HashMap<String, VoteCount> = HashMap::new();
-
-            let re = Regex::new(r"([^\- ]+|\(.+?\))(\+\+|--)").unwrap();
-
-            for cap in re.captures_iter(body) {
-                let ent = cap[1].trim_matches(|c| c == '(' || c == ')').to_string();
-                let e = votes.entry(ent).or_insert(VoteCount{up: 0, down: 0});
-
-                if &cap[2] == "++" {
-                    (*e).up += 1;
-                }
-
-                if &cap[2] == "--" {
-                    (*e).down += 1;
-                }
+            if &cap[2] == "--" {
+                (*e).down += 1;
             }
 
-            for (k, v) in votes.iter() {
-                println!("{} up: {} down: {}", &k, v.up, v.down);
-                self.vote_db.vote(&event.sender, k, v.up, v.down);
+            // Limit max karma per line to 10 (to help prevent spam)
+            if ((*e).up + (*e).down) >= 10 {
+                break;
             }
+        }
+
+        for (k, v) in votes.iter() {
+            self.vote_db.vote(&event.sender, k, v.up, v.down);
         }
     }
 }
