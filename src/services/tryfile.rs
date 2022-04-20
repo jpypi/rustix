8a8d40b890784e7f2d51c4bae883550406f3a142
James Jenkins <jpycoder@gmail.com>
2022-04-20 03:06:40 -0500

    Clean up tryfile path handling
    - Closes #4

--- src/services/tryfile.rs
+++ src/services/tryfile.rs
@@ -1,5 +1,5 @@
-use std::fs::File;
-use std::fs;
+use std::fs::{self, File};
+use std::path::PathBuf;
 use std::io::{BufReader, BufRead};
 
 use rand::SeedableRng;
@@ -14,19 +14,24 @@ use crate::services::utils::reservoir_sample;
 pub struct TryFile {
     safe_re: Regex,
     rng: SmallRng,
-    directory: String
+    directory: PathBuf
 }
 
 
 impl TryFile {
     pub fn new(config: Option<&Value>) -> Self {
+        let dir_string = config.and_then(|c| c.get("directory")
+                                              .and_then(|d| d.as_str())
+                                              .map(|s| s.to_string()))
+                               .unwrap_or("var".to_string());
+        let path = PathBuf::from(dir_string)
+            .canonicalize()
+            .expect("Error validating directory for tryfile");
+
         Self {
             safe_re: Regex::new(r"[a-zA-Z]").unwrap(),
             rng: SmallRng::from_entropy(),
-            directory: config.and_then(|c| c.get("directory")
-                                            .and_then(|d| d.as_str())
-                                            .map(|s| s.to_string()))
-                             .unwrap_or("var".to_string()),
+            directory: path,
         }
     }
 }
@@ -37,8 +42,19 @@ impl<'a> Node<'a> for TryFile {
         let body = event.raw_event.content["body"].as_str().unwrap();
 
         if self.safe_re.is_match(body) {
-            let fname = format!("{}/{}.txt", &self.directory, body);
-            match File::open(fname) {
+            // Build path to file to try
+            // canonicalize normalizes the path and validates that it exists
+            let path = match (&self.directory).join(body).with_extension("txt").canonicalize() {
+                Ok(p) => p,
+                Err(_) => return,
+            };
+
+            // Make sure any accessed file is a child of the config directory
+            if !path.starts_with(&self.directory) {
+                return;
+            }
+
+            match File::open(path) {
                 Ok(d) => {
                     let reader = BufReader::new(d);
                     if let Ok(v) = reservoir_sample(reader.lines(), &mut self.rng) {
@@ -51,7 +67,7 @@ impl<'a> Node<'a> for TryFile {
     }
 
     fn description(&self) -> Option<String> {
-        match fs::read_dir(format!("{}", &self.directory)) {
+        match fs::read_dir(&self.directory) {
             Ok(paths) => {
                 Some(paths.map(|p| {
                                     let mut path = p.unwrap().path();
