72a8015a8c8bac6d9d7ff9ded4cbceb92c0d49a1
James Jenkins <jpycoder@gmail.com>
2023-08-16 14:59:33 -0500

    Add help command to nodectrl

--- src/services/nodectrl/configure.rs
+++ src/services/nodectrl/configure.rs
@@ -1,11 +1,16 @@
-use crate::bot::{Bot, Node, RoomEvent};
+use std::any::Any;
+
+use crate::{bot::{Bot, Node, RoomEvent}, utils::codeblock_format};
 
 pub struct Configure {
+    reply_id: Option<String>,
 }
 
 impl Configure {
     pub fn new() -> Self {
-        Self {  }
+        Self {
+            reply_id: None
+        }
     }
 }
 
@@ -32,9 +37,37 @@ impl<'a> Node<'a> for Configure {
                                         });
             }
         }
+
+        if let Some(command) = body.strip_prefix("node help") {
+            let target = command.trim();
+            bot.delay_service_query("nodectl",
+                                    Some(target.to_string()),
+                                    move |_, n| {
+                                        Box::new(n.configure_description())
+                                    });
+            self.reply_id = Some(event.room_id.to_string());
+        }
+    }
+
+    fn recieve_all_node_post(&mut self, bot: &Bot, result: Vec<(&str, Box<dyn Any>)>) {
+        if let Some(ref rid) = self.reply_id {
+            if let Some((_, value)) = result.into_iter().next() {
+                let mut opt_v = value.downcast::<Option<String>>().unwrap();
+                if let Some(v) = opt_v.take() {
+                    bot.say_fmt(rid, &codeblock_format(&v), &v).ok();
+                } else {
+                    bot.say(rid, &"No config help found.").ok();
+                }
+            } else {
+                bot.say(rid, &"Node not found.").ok();
+            }
+        }
+
+        self.reply_id = None;
     }
 
     fn description(&self) -> Option<String> {
-        Some("node config <node name> <command> - send command to a specific node".to_string())
+        Some("node config <node name> <command> - send command to a specific node\n\
+              node help <node name> - get help for configuring node".to_string())
     }
 }
