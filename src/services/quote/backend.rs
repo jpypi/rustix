dadf58284964c2503e7452d96cf9b3507299df8f
James Jenkins <jpycoder@gmail.com>
2023-11-15 14:07:50 -0600

    Add quote_by method to quote backend

--- src/services/quote/backend.rs
+++ src/services/quote/backend.rs
@@ -3,17 +3,19 @@ use std::time::SystemTime;
 
 use rand::{SeedableRng, Rng};
 use rand::rngs::SmallRng;
+use rand::seq::SliceRandom;
 
 use dotenv::dotenv;
 use diesel::prelude::*;
 use diesel::pg::PgConnection;
 
+
 use super::super::db::user::{self, *};
 use super::super::db::schema::{
     users as us,
+    users::dsl::users,
     quotes as qu,
     quotes::dsl::*,
-    users::dsl::*,
 };
 use super::models::*;
 
@@ -56,8 +58,8 @@ impl Backend {
     pub fn get_quote(&self, quote_id: i32) -> QueryResult<(User, Quote)> {
         let qres: Quote = quotes.filter(qu::dsl::id.eq(quote_id))
                                         .first(&self.connection)?;
-        let ures = users.filter(us::dsl::id.eq(qres.quoter_id))
-                            .get_result(&self.connection)?;
+        let ures = us::dsl::users.filter(us::dsl::id.eq(qres.quoter_id))
+                                 .get_result(&self.connection)?;
         Ok((ures, qres))
     }
 
@@ -76,8 +78,8 @@ impl Backend {
 
         // Try to query for a quote using a random offset
         let qres: Quote = quotes.offset(offset).first(&self.connection)?;
-        let ures = users.filter(us::dsl::id.eq(qres.quoter_id))
-                            .first(&self.connection)?;
+        let ures = us::dsl::users.filter(us::dsl::id.eq(qres.quoter_id))
+                                 .first(&self.connection)?;
         Ok((ures, qres))
     }
 
@@ -102,5 +104,20 @@ impl Backend {
     pub fn search_quotes(&mut self, text: &str) -> QueryResult<Vec<Quote>> {
         let qfilter = quotes.filter(qu::dsl::value.ilike(format!("%{}%", text)));
         Ok(qfilter.load(&self.connection)?)
+    }
+
+    pub fn quote_by(&mut self, user_id: &str) -> QueryResult<(User, Quote)> {
+        let res: Result<Vec<(User, Quote)>, diesel::result::Error> =
+            quotes.inner_join(us::table)
+                  .filter(us::user_id.eq(user_id))
+                  .select((us::all_columns, qu::all_columns))
+                  .load(&self.connection);
+
+        if let Ok(quotes_res) = res {
+            if let Some((u, q)) = quotes_res.choose(&mut self.rng) {
+                return Ok((u.clone(), q.clone()));
+            }
+        }
+        Err(diesel::result::Error::NotFound)
     }
 }
