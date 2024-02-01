7e43f3755b775af7f01ae56c8d87a8271adea527
James Jenkins <jpycoder@gmail.com>
2024-02-01 02:22:46 -0600

    Clean up ShowKarma; make it more idiomatic

--- src/services/karma/show_karma.rs
+++ src/services/karma/show_karma.rs
@@ -4,59 +4,39 @@ use crate::bot::{Bot, Node, RoomEvent};
 
 use super::backend::Backend;
 
-pub struct ShowKarma<'a> {
+pub struct ShowKarma {
     vote_db: Backend,
-    children: Vec<&'a str>,
+    karma_re: Regex,
 }
 
-impl<'a> ShowKarma<'a> {
+impl ShowKarma {
     pub fn new() -> Self {
         Self {
             vote_db: Backend::new(),
-            children: Vec::new(),
+            karma_re: Regex::new(r"^karma (.+)").unwrap(),
         }
     }
 }
 
-impl<'a> Node<'a> for ShowKarma<'a> {
-    fn children(&self) -> Option<&Vec<&'a str>> {
-        Some(&self.children)
-    }
-
-    fn register_child(&mut self, name: &'a str) {
-        self.children.push(name);
-    }
-
+impl<'a> Node<'a> for ShowKarma {
     fn handle(&mut self, bot: &Bot, event: RoomEvent) {
-        let revent = &event.raw_event;
-        if revent.type_ == "m.room.message" &&
-            revent.content["msgtype"] == "m.text" {
-            let body = revent.content["body"].as_str().unwrap();
+        if event.is_normal() {
+            let body = &event.raw_event.content["body"].as_str().unwrap();
 
-            let check_re = Regex::new(r"^karma (.+)").unwrap();
-
-            let mut finds = 0;
-            for cap in check_re.captures_iter(body) {
-                finds += 1;
+            for cap in self.karma_re.captures_iter(body) {
                 let query = cap[1].trim();
-                let res = self.vote_db.get_upvotes(query);
-                if let Ok(Some(k)) = res {
-                    let positive = (k.total_up as f32/(k.total_up+k.total_down) as f32)*100.0;
-                    let total = k.total_up - k.total_down;
-                    let response = format!("Karma for '{}': Net karma: {} (+{}/-{}; {:.1}% like it)",
-                        query, total, k.total_up, k.total_down, positive
-                    );
-                    bot.reply(&event, &response).ok();
-                } else if let Ok(None) = res {
-                    let response = format!("Karma for '{}': Net karma: 0 (+0/-0 0% like it)", query);
-                    bot.reply(&event, &response).ok();
-                } else {
-                    bot.reply(&event, &format!("Error querying karma for '{}'", query)).ok();
-                }
-            }
-
-            if finds == 0 {
-                self.propagate_event(bot, &event);
+                let res = match self.vote_db.get_upvotes(query) {
+                    Ok(Some(k)) => {
+                        let positive = (k.total_up as f32/(k.total_up+k.total_down) as f32)*100.0;
+                        let total = k.total_up - k.total_down;
+                        format!("Karma for '{}': Net karma: {} (+{}/-{}; {:.1}% like it)",
+                            query, total, k.total_up, k.total_down, positive
+                        )
+                    },
+                    Ok(None) => format!("Karma for '{}': Net karma: 0 (+0/-0 0% like it)", query),
+                    _ => format!("Error querying karma for '{}'", query),
+                };
+                bot.reply(&event, &res).ok();
             }
         }
     }
